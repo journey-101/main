7123f1326d21c6cf510a949727342c0ff3ff97b8
designpablo1-lab <design.pablo1@gmail.com>
2026-06-26 16:08:51 +0900

    fix: rename MapComponent

--- frontend/src/features/map/MapComponent.tsx
+++ frontend/src/features/map/MapComponent.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Map, MapMarker } from "react-kakao-maps-sdk";
 
-export default function MapContainer() {
+export function MapComponent() {
   return (
     <Map
       center={{ lat: 37.5665, lng: 126.9780 }} // 지도 중심 좌표 (서울시청)
       style={{
-        width: "100%",
+        width: "400px",
         height: "500px", // 원하는 지도 높이 설정
       }}
       level={3} // 지도 확대 레벨
